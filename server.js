30ebe399699f612cb78d37d3d979009b61a03668
yatmin <273117832@qq.com>
2017-10-15 21:51:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/api/user',function(req,res){
     });
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
